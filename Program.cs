086f7b00b67a5a464daeeeab0b22120c1955c426
Ioana Popa <ioana.popa@snyk.io>
2026-01-13 22:36:42 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+namespace Repro
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Empty - no references to Hosting or WebEnvironment
+        }
+    }
+}
